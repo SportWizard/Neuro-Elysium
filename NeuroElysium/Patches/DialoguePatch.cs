70825546bd0970a025f25868aee3deda0758dbce
SportWizard <45925659+SportWizard@users.noreply.github.com>
2026-01-22 16:37:52 -0500

    Dialogue patch

---
+++ NeuroElysium/Patches/DialoguePatch.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+using PixelCrushers.DialogueSystem;
+using System;
+
+namespace NeuroElysium.Patches;
+
+[HarmonyPatch(typeof(Conversation), "GetDialogueEntry", new Type[] { typeof(int) })]
+internal class DialoguePatch {
+    [HarmonyPostfix]
+    static void CurrentDialogue(DialogueEntry __result) {
+        Plugin.Log.LogInfo(__result.currentDialogueText);
+    }
+}
