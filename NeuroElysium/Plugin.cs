e6476d28f1ce13bff552e9dd73937ca1d45b3fa5
Henry Ye Cao <45925659+SportWizard@users.noreply.github.com>
2026-01-22 16:37:52 -0500

    Dialogue patch

---
+++ NeuroElysium/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using NeuroElysium.Patches;
+
+namespace NeuroElysium;
+
+[BepInPlugin("com.sportwizard.neuroelysium", "Neuro Elysium", "1.0.0")]
+[BepInProcess("Disco Elysium.exe")]
+public class Plugin : BasePlugin {
+    internal static new ManualLogSource Log;
+
+    public override void Load() {
+        Log = base.Log;
+
+        Harmony harmony = new Harmony("BepInEx_Patch");
+        harmony.PatchAll(typeof(DialoguePatch));
+    }
+}
