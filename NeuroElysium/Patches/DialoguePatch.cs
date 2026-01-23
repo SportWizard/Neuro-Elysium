e47731c09ba37d556159e719ddb02bb3f41b0061
Henry Ye Cao <45925659+SportWizard@users.noreply.github.com>
2026-01-23 05:25:13 -0500

    Use patches for getting dialogue and responses

---
+++ NeuroElysium/Patches/DialoguePatch.cs
@@ -0,0 +1,12 @@
+﻿using HarmonyLib;
+using PixelCrushers.DialogueSystem;
+
+namespace NeuroElysium.Patches;
+
+internal class DialoguePatch {
+    [HarmonyPatch(typeof(ConversationView), "StartSubtitle")]
+    [HarmonyPrefix]
+    static void StartSubtitlePrefix(Subtitle subtitle, bool isPCResponseMenuNext, bool isPCAutoResponseNext) {
+        Plugin.Log.LogInfo(subtitle.dialogueEntry.currentDialogueText);
+    }
+}
