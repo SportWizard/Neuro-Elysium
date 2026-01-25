6db251dffebe581600654ac07bdac5a40a0488dc
Henry Ye Cao <45925659+SportWizard@users.noreply.github.com>
2026-01-24 21:37:25 -0500

    Prevent sending locked responses

--- NeuroElysium/Patches/ResponsesPatch.cs
+++ NeuroElysium/Patches/ResponsesPatch.cs
@@ -8,24 +8,29 @@ namespace NeuroElysium.Patches;
 
 [HarmonyPatch]
 internal class ResponsesPatch {
-    private static List<SunshineResponseButton> buttons = [];
+    private static List<SunshineResponseButton> _buttons = [];
+    private static int _lockedButton = 0;
 
     [HarmonyPatch(typeof(SunshineResponseButton), "OnEnable")]
     [HarmonyPostfix]
     static void SunshineResponseButtonPostfix(ref SunshineResponseButton __instance) {
-        if (__instance == null)
+        if (__instance == null ||DialogueManager.CurrentConversationState == null || DialogueManager.CurrentConversationState.pcResponses == null)
             return;
 
-        buttons.Add(__instance);
+        if (__instance.response.enabled)
+            _buttons.Add(__instance);
+        else
+            _lockedButton++;
 
         // Since each instance is only one response. Need to collect all the response before sending to Neuro
-        if (DialogueManager.currentConversationState.pcResponses.Length == buttons.Count) {
+        if (DialogueManager.CurrentConversationState.pcResponses.Length - _lockedButton == _buttons.Count) {
             ActionWindow.Create(GO.PluginObject)
                 .SetForce(0, "Choose a response", "", false)
-                .AddAction(new ChooseResponseAction(ref buttons))
+                .AddAction(new ChooseResponseAction(ref _buttons))
                 .Register();
 
-            buttons = [];
+            _buttons = [];
+            _lockedButton = 0;
         }
     }
 }
